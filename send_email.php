5a8d17b2566632adc0e51071e4652c48ef1ff3a4
SRIYANS RAJ <148074357+SRIYANSRAJ@users.noreply.github.com>
2024-09-07 14:27:33 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,9 +3,7 @@ try {
     //Server settings
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';  // Gmail SMTP server
-    $mail->SMTPAuth = true;
-    $mail->Username = 'sriyansraj02@gmail.com';  // Your Gmail address
-    $mail->Password = 'St@115139';  // Your Gmail password or app-specific password
+    $mail->SMTPAuth';  // Your Gmail password or app-specific password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;  // TCP port to connect to, use 465 for `ssl`
     
